9b87f38b822f83de33c455a9051deed50caf777b
jayatubi <yzk0370@gmail.com>
2018-06-29 18:01:15 +0800

    variable naming

--- build/interface.c
+++ build/interface.c
@@ -23,17 +23,17 @@ LZ4FLIB_API int Unity_LZ4_uncompressSize(const char* srcBuffer, size_t srcSize)
     LZ4F_createDecompressionContext(&dctx, LZ4F_getVersion());
     LZ4F_frameInfo_t frameInfo = { 0 };
     LZ4F_getFrameInfo(dctx, &frameInfo, srcBuffer, &srcSize);
-    int contentSize = frameInfo.contentSize;
+    int uncompressSize = frameInfo.contentSize;
     LZ4F_freeDecompressionContext(dctx);
-    return contentSize;
+    return uncompressSize;
 }
 
 LZ4FLIB_API int Unity_LZ4_decompress(const char* src, size_t srcSize, char* dst, size_t dstCapacity)
 {
     LZ4F_dctx* dctx;
     LZ4F_createDecompressionContext(&dctx, LZ4F_getVersion());
-    int uncompressSize = LZ4F_decompress(dctx, dst, &dstCapacity, src, &srcSize, 0);
+    int errorCode = LZ4F_decompress(dctx, dst, &dstCapacity, src, &srcSize, 0);
     LZ4F_freeDecompressionContext(dctx);
-    return uncompressSize;
+    return errorCode;
 }
 
