3798cbfb30b78fee8299cdaeba94b7deab571de8
jayatubi <yzk0370@gmail.com>
2018-06-29 14:53:43 +0800

    deprecate x86

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@ INSTALL_TARGET=$1
 cp plugin/Plugins/iOS/liblz4.a $INSTALL_TARGET/Assets/Plugins/iOS
 cp -r plugin/Plugins/lz4.bundle $INSTALL_TARGET/Assets/Plugins/macOS
 cp -r plugin/Plugins/Android/libs/ $INSTALL_TARGET/Assets/Plugins/Android/libs
-cp plugin/Plugins/x86/lz4.dll $INSTALL_TARGET/Assets/Plugins/Windows/x86
 cp plugin/Plugins/x86_64/lz4.dll $INSTALL_TARGET/Assets/Plugins/Windows/x86_64
--- make_win32.bat
+++
@@ -1,7 +0,0 @@
-mkdir build32 & pushd build32
-cmake -G "Visual Studio 14 2015" ..
-popd
-cmake --build build32 --config Debug
-md plugin\Plugins\x86
-copy /Y build32\Debug\lz4.dll plugin\Plugins\x86\lz4.dll
-pause
