ed01f97d1484c7e053f622593a3590b80e52d22f
jayatubi <yzk0370@gmail.com>
2018-06-29 20:56:49 +0800

    windows symbols export

--- build/make_win64.bat
+++ build/make_win64.bat
@@ -1,7 +1,7 @@
-mkdir build_64 & pushd build_64
+mkdir build_x86_64 & pushd build_x86_64
 cmake -G "Visual Studio 14 2015 Win64" ..
 popd
-cmake --build build_64 --config Debug
-md plugin\Plugins\x86_64
-copy /Y build64\Debug\lz4.dll plugin\Plugins\x86_64\lz4.dll
+cmake --build build_x86_64 --config Debug
+md Plugins\x86_64
+copy /Y build_x86_64\Debug\lz4.dll Plugins\x86_64\lz4.dll
 pause
